68db5b1bc70a490235a1977e839643c2eb8b3bb2
KomodoTech <alexandre.leibler@gmail.com>
2022-03-01 17:54:39 -0500

    ready to build

---
+++ server/models/cardModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const PORT = 27017;
+const MONGO_URI = `mongodb://localhost:${PORT}`;
+
+mongoose.connect(MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  dbName: 'cardbattler',
+})
+  .then(() => { console.log('Connected to Mongo database'); })
+  .catch((err) => {
+    console.log('Error in mongoose connect (model.js): ', err);
+  });
+
+const { Schema } = mongoose;
+
+const cardSchema = new Schema({
+  health: Number,
+  attack: Number,
+  species_id: Number,
+  equipment_id: Number,
+});
+
+// TODO: mongoose middleware
+
+const Card = mongoose.model('card', cardSchema);
+
+module.exports = Card;
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,27 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const webpack = require("webpack");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const mode = process.env.NODE_ENV; //our environment variables are accessible via process.env
-//the && here means run both of these commands (order matters) in package json
+const mode = process.env.NODE_ENV; // our environment variables are accessible via process.env
+// the && here means run both of these commands (order matters) in package json
 // console.log('mode set by environment variable: ', mode);
 
 module.exports = {
   // TODO: use brackets if you need multiple entry points (When would we use multiple entry points?)
-  entry: ["./client/index.js"],
+  entry: ['./client/index.js'],
   // NOTE: webpack will create the build directory for us
   // TODO: check if there's any point to having publicPath in output
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/",
-    filename: "bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+    filename: 'bundle.js',
   },
-  mode: mode, // Production compresses the data, not typical in development, Changing to development allows us to then use webpack dev server that enables hot reloading
+  mode,
+  // Production compresses the data, not typical in development, Changing to development 
+  // allows us to then use webpack dev server that enables hot reloading
   // NOTE: you could have rules for different modules, but here we're applying the same
   // rule everywhere
   devServer: {
-    host: "localhost",
+    host: 'localhost',
     port: 8080,
     // enable HMR on the devServer
     hot: true,
@@ -29,15 +30,16 @@ module.exports = {
 
     static: {
       // match the output path
-      directory: path.resolve(__dirname, "build"), //If we are running development mode, we are ignoring the output: {} method and just serve the static path.
+      directory: path.resolve(__dirname, 'build'), // If we are running development mode, we are ignoring the output: {} method and just serve the static path.
       // match the output 'publicPath'
-      publicPath: "/",
+      publicPath: '/',
     },
-    proxy: {                                   //uncomment the proxy method to allow the frontend to communicate with the proxy
-      "/api": "http://localhost:3000/",
+    proxy: { // uncomment the proxy method to allow the frontend to communicate with the proxy
+      '/api': 'http://localhost:3000/',
     },
   },
-  // TODO: look into modules more (is our app one module? what exactly happens when we exclude node_modules?)
+  // TODO: look into modules more (is our app one module? what exactly happens when we 
+  // exclude node_modules?)
   module: {
     rules: [
       {
@@ -48,31 +50,33 @@ module.exports = {
         test: /.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env", "@babel/preset-react"],
+            presets: ['@babel/preset-env', '@babel/preset-react'],
           },
         },
       },
       {
-        // The definition says css-loader interprets @import and url() like import/require() and will resolve them. What do we mean by this? css-loader will take all the CSS from the CSS file and generate it to a single string and will pass this to style-loader.
+        // The definition says css-loader interprets @import and url() like import/require()
+        // and will resolve them. What do we mean by this? css-loader will take all the CSS from
+        // the CSS file and generate it to a single string and will pass this to style-loader.
         // style-loader will take this string and will embed it in the style tag in index.html.
         test: /.(css|scss)$/,
-        exclude: /node_modules/, //webpack is aware of the minified, uglified js and knows to grab that file, so we can exclude it here
-        use: ["style-loader", "css-loader", "sass-loader"], //order by which you place your css loaders matters //
+        exclude: /node_modules/, // webpack is aware of the minified, uglified js and knows to grab that file, so we can exclude it here
+        use: ['style-loader', 'css-loader', 'sass-loader'], // order by which you place your css loaders matters //
       },
       {
         test: /\.png$/,
         use: [{
-          loader: "url-loader",
+          loader: 'url-loader',
           options: {
-            mimetype: "image/png"
-          }
+            mimetype: 'image/png',
+          },
         }],
       },
       {
         test: /\.svg$/,
-        use: "file-loader",
+        use: 'file-loader',
       }
     ],
   },
@@ -80,7 +84,7 @@ module.exports = {
     // HtmlWebpackPlugin will take the bundle.js file and insert it inline into a
     // index.html file inside the dist/build directory. You can pass in a template page
     new HtmlWebpackPlugin({
-      template: "./index.html", //yes, htmlWebpack only works in development// index html is placed in root in RAM because it is a feature of setting the mode as development.
+      template: './index.html', // yes, htmlWebpack only works in development// index html is placed in root in RAM because it is a feature of setting the mode as development.
     }),
   ],
 };
